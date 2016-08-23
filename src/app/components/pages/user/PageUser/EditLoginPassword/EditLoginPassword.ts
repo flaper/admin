2a15172503d741aa4062826bd40e013f3202d162
doctorMinami <ivan92zakharov@gmail.com>
2016-08-23 19:03:27 +0900

    ADD: messege error or success when changing username/password

--- src/app/components/pages/user/PageUser/EditLoginPassword/EditLoginPassword.ts
+++ src/app/components/pages/user/PageUser/EditLoginPassword/EditLoginPassword.ts
@@ -1,5 +1,5 @@
 import {Component, Input} from "@angular/core";
-import {UserService} from "@flaper/angular";
+import {UserService, ApiService} from "@flaper/angular";
 import {ActivatedRoute} from '@angular/router';
 
 @Component({
@@ -11,8 +11,9 @@ export class EditLoginPassword {
   userId = null;
   user = null;
   data = {};
-
-  constructor(private _user:UserService, route:ActivatedRoute) {
+  errorClass: string;
+  error: string;
+  constructor(private _user:UserService, route:ActivatedRoute, api:ApiService) {
     route.params.subscribe(params => {
       this.userId = params['id'];
     });
@@ -20,11 +21,37 @@ export class EditLoginPassword {
       this.data['id'] = this.userId;
       this.data['username'] = user.username;
     });
+    this.error = api.error_message;
   }
 
   onSubmit() {
-    this._user.put(this.data).subscribe(data => {
-      this.data = data
-    });
+    this._user.put(this.data).subscribe(
+      data => {
+        this.data = data;
+      },
+      err => {
+        this.logErr(err);
+      },
+      () => {
+        this.done()
+      }
+    );
+  }
+  done(){
+    this.errorClass = "info";
+    this.error = "Username и password успешно измненены";
+  }
+
+  logErr(err){
+    let status = err.status;
+    this.errorClass = "danger";
+    switch(status){
+      case 422:
+        this.error = "Такой username уже существует";
+        break;
+      default:
+        this.error = err.message;
+        break;
+    }
   }
 }
