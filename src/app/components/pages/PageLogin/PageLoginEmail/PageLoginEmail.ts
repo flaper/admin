ae8e39be19f7ab081aaf5b0ca6ac6e4e179b4501
doctorMinami <ivan92zakharov@gmail.com>
2016-08-23 15:38:15 +0900

    FIX: login component

--- src/app/components/pages/PageLogin/PageLoginEmail/PageLoginEmail.ts
+++ src/app/components/pages/PageLogin/PageLoginEmail/PageLoginEmail.ts
@@ -1,6 +1,5 @@
-import {Component, Input} from "@angular/core";
-import {UserService, AuthService} from "@flaper/angular";
-import {ActivatedRoute} from '@angular/router';
+import {Component} from "@angular/core";
+import {AuthService} from "@flaper/angular";
 
 @Component({
   selector: "page-login-email",
@@ -8,19 +7,11 @@ import {ActivatedRoute} from '@angular/router';
 })
 
 export class PageLoginEmail {
-  userId = null;
   data = {};
 
-  constructor(private _user:UserService, route:ActivatedRoute, auth:AuthService) {
-    // route.params.subscribe(params => {
-    //   this.userId = params['id'];
-    // });
-    // this._user.getById(this.userId).subscribe(user => { this.data = user});
-    // this.data.login = this.user.login ? "";
+  constructor(private auth:AuthService) {
   }
   onSubmit() {
-    this.auth.login(this.data).subscribe(data => {this.data = data});
-    // this._user.login(this.data);
-    // console.log(this.data);
+    this.auth.login(this.data);
   }
 }
