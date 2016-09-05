49a2d59327d67e2b7ac235a9d9d3f143e314c477
sklyukin <stas.msu@gmail.com>
2016-09-05 23:17:01 +0500

    removing old code

--- src/main.browser.ts
+++ src/main.browser.ts
@@ -8,27 +8,6 @@ LibConfig.Init({
   SUCCESS_LOGIN_CALLBACK: PageService.NavigateAfterLogin
 });
 
-
-/*
- * Vendors
- * For vendors for example jQuery, Lodash, angular2-jwt just import them anywhere in your app
- * You can also import them in vendors to ensure that they are bundled in one file
- * Also see custom-typings.d.ts as you also need to do `typings install x` where `x` is your module
- */
-
-
-/*
- * Hot Module Reload
- * experimental version by @gdi2290
- */
-if ('development' === ENV && HMR === true) {
-  // activate hot module reload
-  let ngHmr = require('angular2-hmr');
-  ngHmr.hotModuleReplacement(main, module);
-} else {
-  // bootstrap when document is ready
-  document.addEventListener('DOMContentLoaded', () => main());
-}
 /*
  * Angular bootstraping
  */
@@ -45,7 +24,7 @@ import { AppModule } from './app/index';
 /*
  * Bootstrap our Angular app with a top level NgModule
  */
-export function main(): Promise<any> {
+export function main():Promise<any> {
   return platformBrowserDynamic()
     .bootstrapModule(AppModule)
     .then(decorateModuleRef)
